9a6622bca688a8125ab1eb4cb5864aa6eb50f01e
Hamza Ali <hamzali4642@gmail.com>
2022-11-22 11:30:44 +0500

    widget utility

--- lib/utility_extensions.dart
+++ lib/utility_extensions.dart
@@ -100,4 +100,10 @@ extension ListUtilities on List {
 
 extension WidgetUtilities on Widget {
   Widget get toSliver => SliverToBoxAdapter(child: this,);
+  Widget removeScrollEffect(){
+    return ScrollConfiguration(
+        behavior: const MaterialScrollBehavior().copyWith(overscroll: false),
+        child : this,
+    );
+  }
 }
