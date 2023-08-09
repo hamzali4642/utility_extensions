cbb54df1cd4cfb6b1d279a593436e69c5b966090
Hamza <hamzali4642@gmail.com>
2023-08-09 10:18:05 +0500

    font utility added

---
+++ lib/extensions/font_utilities.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class FontWeights{
+  static const  thin = FontWeight.w100;
+  static const  extraLight = FontWeight.w200;
+  static const  light = FontWeight.w300;
+  static const  normal = FontWeight.w400;
+  static const  medium = FontWeight.w500;
+  static const  semiBold = FontWeight.w600;
+  static const  bold = FontWeight.w700;
+  static const  extraBold = FontWeight.w800;
+  static const  heavy = FontWeight.w900;
+}
