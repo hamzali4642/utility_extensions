851e6642a03623327f2b145c688d7d23ba4ab3dd
Ahmed <razaraza3536@gmail.com>
2024-02-12 15:14:36 +0500

    add count time in pop in case user wants to pop multiple times in one attempt

--- lib/extensions/context_extensions.dart
+++ lib/extensions/context_extensions.dart
@@ -2,33 +2,45 @@ import 'package:flutter/material.dart';
 
 extension ContextUtilities on BuildContext {
   double get width => MediaQuery.of(this).size.width;
+
   double get height => MediaQuery.of(this).size.height;
 
   double get topPadding => MediaQuery.of(this).padding.top;
+
   double get bottomPadding => MediaQuery.of(this).padding.bottom;
+
   double get leftPadding => MediaQuery.of(this).padding.left;
+
   double get rightPadding => MediaQuery.of(this).padding.right;
 
-  Future<dynamic> push({required Widget child, bool rootNavigator = false}) async {
+  Future<dynamic> push(
+      {required Widget child, bool rootNavigator = false}) async {
     return await Navigator.of(this, rootNavigator: rootNavigator)
         .push(MaterialPageRoute(builder: (ctx) => child));
   }
 
-  Future<dynamic> pushReplacement({required Widget child, bool rootNavigator = false}) async {
+  Future<dynamic> pushReplacement(
+      {required Widget child, bool rootNavigator = false}) async {
     return await Navigator.of(this, rootNavigator: rootNavigator)
         .pushReplacement(MaterialPageRoute(builder: (ctx) => child));
   }
 
-  Future<dynamic> pushAndRemoveUntil({required Widget child, bool rootNavigator = false}) async {
-    return await Navigator.of(this, rootNavigator: rootNavigator).pushAndRemoveUntil(
+  Future<dynamic> pushAndRemoveUntil(
+      {required Widget child, bool rootNavigator = false}) async {
+    return await Navigator.of(this, rootNavigator: rootNavigator)
+        .pushAndRemoveUntil(
       MaterialPageRoute(builder: (ctx) => child),
-          (route) => false,
+      (route) => false,
     );
   }
 
-  bool  canPop() => Navigator.of(this).canPop();
+  bool canPop() => Navigator.of(this).canPop();
 
-  void pop({dynamic data, bool rootNavigator = false}) {
-    Navigator.of(this, rootNavigator: rootNavigator).pop(data);
+  void pop({dynamic data, bool rootNavigator = false, count = 1}) {
+    int popCount = 0;
+    do {
+      Navigator.of(this, rootNavigator: rootNavigator).pop(data);
+      popCount++;
+    } while (popCount == count);
   }
 }
