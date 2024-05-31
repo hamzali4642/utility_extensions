005b7db49d5275c9cfc247accd51581940a6b978
Sameer <sameerghaffar20@gmail.com>
2024-05-31 10:21:12 +0500

    material and widget state extention added

--- lib/extensions/font_utilities.dart
+++ lib/extensions/font_utilities.dart
@@ -2,13 +2,22 @@
 import 'package:flutter/material.dart';
 
 class FontWeights{
+  /// FontWeight = w100
   static const  thin = FontWeight.w100;
+  /// FontWeight = w200
   static const  extraLight = FontWeight.w200;
+  /// FontWeight = w300
   static const  light = FontWeight.w300;
+  /// FontWeight = w400
   static const  normal = FontWeight.w400;
+  /// FontWeight = w500
   static const  medium = FontWeight.w500;
+  /// FontWeight = w600
   static const  semiBold = FontWeight.w600;
+  /// FontWeight = w700
   static const  bold = FontWeight.w700;
+  /// FontWeight = w800
   static const  extraBold = FontWeight.w800;
+  /// FontWeight = w900
   static const  heavy = FontWeight.w900;
 }
