6eb3d11529c9f339b15beda57a219c948ea35797
Hamza <hamzali4642@gmail.com>
2025-09-09 12:57:04 +0500

    color, widgets utilities

--- lib/extensions/color_utilities.dart
+++ lib/extensions/color_utilities.dart
@@ -5,4 +5,29 @@ extension ColorUtilities on Color{
     assert(opacity >= 0.0 && opacity <= 1.0);
     return withAlpha((opacity * 255).toInt());
   }
+
+  MaterialColor getMaterialColor() {
+    return MaterialColor(toARGB32(), getSwatch(this));
+  }
+
+  Map<int, Color> getSwatch(Color color) {
+    final hslColor = HSLColor.fromColor(color);
+    final lightness = hslColor.lightness;
+    const lowDivisor = 6;
+    const highDivisor = 5;
+    final lowStep = (1.0 - lightness) / lowDivisor;
+    final highStep = lightness / highDivisor;
+    return {
+      50: (hslColor.withLightness(lightness + (lowStep * 5))).toColor(),
+      100: (hslColor.withLightness(lightness + (lowStep * 4))).toColor(),
+      200: (hslColor.withLightness(lightness + (lowStep * 3))).toColor(),
+      300: (hslColor.withLightness(lightness + (lowStep * 2))).toColor(),
+      400: (hslColor.withLightness(lightness + lowStep)).toColor(),
+      500: (hslColor.withLightness(lightness)).toColor(),
+      600: (hslColor.withLightness(lightness - highStep)).toColor(),
+      700: (hslColor.withLightness(lightness - (highStep * 2))).toColor(),
+      800: (hslColor.withLightness(lightness - (highStep * 3))).toColor(),
+      900: (hslColor.withLightness(lightness - (highStep * 4))).toColor(),
+    };
+  }
 }
--- lib/extensions/widget_utilities.dart
+++ lib/extensions/widget_utilities.dart
@@ -24,6 +24,20 @@ extension WidgetUtilities on Widget {
       child : this,
     );
   }
+
+  Widget applySymmetricPadding({double vertical = 0, double horizontal = 0}){
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: horizontal, vertical: vertical),
+      child : this,
+    );
+  }
+
+  Widget applyPadding({double top = 0, double bottom = 0, double left = 0, double right = 0}){
+    return Padding(
+      padding: EdgeInsets.only(top: top, bottom: bottom, left: left, right: right),
+      child : this,
+    );
+  }
 }
 
 extension WidgetUtilitiesNumbers on num{
